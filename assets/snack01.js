a36acc270f0e64b0d9158dd9927b5531236ca035
MrGaggio <steruggiu@gmail.com>
2021-10-18 14:30:43 +0100

    creata struttura e fatto primo snack

---
+++ assets/snack01.js
@@ -0,0 +1,14 @@
+/* L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore. */
+
+let numberOne = prompt("Digita un numero")
+let numberTwo = prompt('Digita un altro numero')
+
+if (numberOne > numberTwo) {
+    console.log(numberOne);
+} else if(numberTwo > numberOne){
+    console.log(numberTwo);
+} else if( numberOne = numberTwo)
+    console.log("try again");
+
+
